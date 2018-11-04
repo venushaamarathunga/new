932528d473163950a402ad5f940b3636b1ffa22e
Venusha Amarathinga <avenusha95@gmail.com>
2018-11-04 22:19:16 +0530

    Added this day first

---
+++ src/app/shared/services/repository.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { RepositoryService } from './repository.service';
+
+
+describe('RepositoryService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [RepositoryService]
+    });
+  });
+
+  it('should be created', inject([RepositoryService], (service: RepositoryService) => {
+    expect(service).toBeTruthy();
+  }));
+});
